1c10a1fd64f92dee1c4a0d24ee5a7f5a30d2a2ed
Nickmasta <58677778+Nickmasta@users.noreply.github.com>
2025-04-15 14:27:07 -0700

    Update model.py

--- model.py
+++ model.py
@@ -51,7 +51,7 @@ class PixelCNNLayer_down(nn.Module):
 
 
 class PixelCNN(nn.Module):
-    def __init__(self, nr_resnet=5, nr_filters=160, nr_logistic_mix=10, resnet_nonlinearity='concat_elu', input_channels=3, num_classes=4, embedding_dim=16):
+    def __init__(self, nr_resnet=3, nr_filters=80, nr_logistic_mix=10, resnet_nonlinearity='concat_elu', input_channels=3, num_classes=4, embedding_dim=16):
         # Added the number of classes in the definition as well as embedding dimension
         super(PixelCNN, self).__init__()
         if resnet_nonlinearity == 'concat_elu' :
