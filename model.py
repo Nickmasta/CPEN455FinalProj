900243b9372d6ef90b369094a4655d36e15defd4
Nickmasta <58677778+Nickmasta@users.noreply.github.com>
2025-04-15 19:14:14 -0700

    Update model.py

--- model.py
+++ model.py
@@ -51,9 +51,9 @@ class PixelCNNLayer_down(nn.Module):
 
 
 class PixelCNN(nn.Module):
-    def __init__(self, nr_resnet=4, nr_filters=100, nr_logistic_mix=10,
+    def __init__(self, nr_resnet=5, nr_filters=80, nr_logistic_mix=10,
                 resnet_nonlinearity='concat_elu', input_channels=3, 
-                num_classes=4, embedding_dim=16):
+                num_classes=4, embedding_dim=32):
         super(PixelCNN, self).__init__()
         if resnet_nonlinearity == 'concat_elu':
             self.resnet_nonlinearity = lambda x: concat_elu(x)
@@ -62,10 +62,7 @@ class PixelCNN(nn.Module):
         
         self.num_classes = num_classes
         self.embedding_dim = embedding_dim
-        
-        # Early fusion modifications
         self.class_embedding = nn.Embedding(num_classes, embedding_dim)
-        # Project embedding to match spatial dimensions
         self.embed_proj = nn.Conv2d(embedding_dim, nr_filters, kernel_size=1)
         
         self.nr_filters = nr_filters
@@ -74,17 +71,34 @@ class PixelCNN(nn.Module):
         self.right_shift_pad = nn.ZeroPad2d((1, 0, 0, 0))
         self.down_shift_pad = nn.ZeroPad2d((0, 0, 1, 0))
 
+        # Down layers
         down_nr_resnet = [nr_resnet] + [nr_resnet + 1] * 2
         self.down_layers = nn.ModuleList([PixelCNNLayer_down(down_nr_resnet[i], nr_filters,
                                         self.resnet_nonlinearity) for i in range(3)])
 
+        # Up layers
         self.up_layers = nn.ModuleList([PixelCNNLayer_up(nr_resnet, nr_filters,
-                                        self.resnet_nonlinearity) for _ in range(3)])
+                                      self.resnet_nonlinearity) for _ in range(3)])
 
-        # Modify initial convolutions to account for embedding
+        # Downsampling streams 
+        self.downsize_u_stream = nn.ModuleList([
+            down_shifted_conv2d(nr_filters, nr_filters, stride=(2,2)) for _ in range(2)
+        ])
+        self.downsize_ul_stream = nn.ModuleList([
+            down_right_shifted_conv2d(nr_filters, nr_filters, stride=(2,2)) for _ in range(2)
+        ])
+
+        # Upsampling streams
+        self.upsize_u_stream = nn.ModuleList([
+            down_shifted_deconv2d(nr_filters, nr_filters, stride=(2,2)) for _ in range(2)
+        ])
+        self.upsize_ul_stream = nn.ModuleList([
+            down_right_shifted_deconv2d(nr_filters, nr_filters, stride=(2,2)) for _ in range(2)
+        ])
+
+        # Initial convolutions with extra channel for class embedding
         self.u_init = down_shifted_conv2d(input_channels + 1 + nr_filters, nr_filters, 
                                         filter_size=(2,3), shift_output_down=True)
-
         self.ul_init = nn.ModuleList([
             down_shifted_conv2d(input_channels + 1 + nr_filters, nr_filters,
                               filter_size=(1,3), shift_output_down=True),
@@ -105,31 +119,41 @@ class PixelCNN(nn.Module):
             padding = torch.ones(x.size(0), 1, x.size(2), x.size(3), device=x.device)
             x = torch.cat((x, padding), 1)
 
-        ### Early Fusion ###
+        # Early fusion
         if class_labels is not None:
-            # Get class embeddings
-            h_class = self.class_embedding(class_labels)  # [B, embedding_dim]
-            # Project to nr_filters and expand spatially
-            h_class = self.embed_proj(h_class.unsqueeze(-1).unsqueeze(-1))  # [B, nr_filters, 1, 1]
-            h_class = h_class.expand(-1, -1, x.size(2), x.size(3))  # [B, nr_filters, H, W]
+            h_class = self.class_embedding(class_labels)
+            h_class = self.embed_proj(h_class.unsqueeze(-1).unsqueeze(-1))
+            h_class = h_class.expand(-1, -1, x.size(2), x.size(3))
         else:
-            # If no class provided, use zeros
-            h_class = torch.zeros(x.size(0), self.nr_filters, x.size(2), x.size(3), device=x.device)
+            h_class = torch.zeros(x.size(0), self.nr_filters, x.size(2), x.size(3), 
+                      device=x.device)
 
-        # Prepare input with padding and class information
         x = x if sample else torch.cat((x, self.init_padding), 1)
-        x = torch.cat((x, h_class), dim=1)  # Concatenate along channel dimension
+        x = torch.cat((x, h_class), dim=1)
+
+        import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+from layers import *
+
+class PixelCNN(nn.Module):
+    # ... [keep all your existing __init__ code] ...
+
+    def forward(self, x, class_labels=None, sample=False):
+        # ... [keep your existing forward code until the up pass] ...
 
         ### UP PASS ###
         u_list = [self.u_init(x)]
         ul_list = [self.ul_init[0](x) + self.ul_init[1](x)]
         
         for i in range(3):
+            # Middle fusion could be added here if desired
             u_out, ul_out = self.up_layers[i](u_list[-1], ul_list[-1])
             u_list += u_out
             ul_list += ul_out
 
-            if i != 2:
+            if i != 2:  # Only downsample first two layers
                 u_list += [self.downsize_u_stream[i](u_list[-1])]
                 ul_list += [self.downsize_ul_stream[i](ul_list[-1])]
 
@@ -138,6 +162,7 @@ class PixelCNN(nn.Module):
         ul = ul_list.pop()
 
         for i in range(3):
+            # Middle fusion could be added here if desired
             u, ul = self.down_layers[i](u, ul, u_list, ul_list)
 
             if i != 2:
@@ -146,20 +171,17 @@ class PixelCNN(nn.Module):
 
         x_out = self.nin_out(F.elu(ul))
         return x_out
-        
-def sample(self, batch_size, obs_shape, class_labels=None):
+
+    def sample(self, batch_size, obs_shape, class_labels=None):
         """Generate samples from the model"""
         channels, height, width = obs_shape
         samples = torch.zeros(batch_size, channels, height, width).to(next(self.parameters()).device)
-        
-        # Initialize with zeros (or random noise if preferred)
-        samples.fill_(0)
         
         # Add initial padding channel
         padding = torch.ones(batch_size, 1, height, width).to(samples.device)
         samples = torch.cat((samples, padding), dim=1)
         
-        # Prepare class conditioning if provided
+        # Prepare class conditioning
         if class_labels is not None:
             h_class = self.class_embedding(class_labels)
             h_class = self.embed_proj(h_class.unsqueeze(-1).unsqueeze(-1))
@@ -167,26 +189,26 @@ def sample(self, batch_size, obs_shape, class_labels=None):
         else:
             h_class = torch.zeros(batch_size, self.nr_filters, height, width).to(samples.device)
         
-        # Concatenate class information
-        samples = torch.cat((samples, h_class), dim=1)
-        
         # Pixel-by-pixel generation
         with torch.no_grad():
             for i in range(height):
                 for j in range(width):
                     for c in range(channels):
-                        # Get model output
-                        out = self.forward(samples[:, :channels, :, :], 
-                                         class_labels=class_labels, 
-                                         sample=True)
+                        # Prepare input with current samples and conditioning
+                        current_input = torch.cat([
+                            samples[:, :channels, :, :],  # Current pixel values
+                            padding,                      # Padding channel
+                            h_class                      # Class conditioning
+                        ], dim=1)
                         
-                        # Sample from output distribution (modify based on your output format)
-                        out_sample = torch.argmax(out[:, :, i, j], dim=1)
+                        # Get model output
+                        out = self.forward(current_input, class_labels=None, sample=True)
+                        
+                        # Sample from output distribution (adjust based on your output)
+                        # For logistic mixture: need proper sampling implementation
+                        out_sample = torch.argmax(out[:, :, i, j], dim=1).float()
                         samples[:, c, i, j] = out_sample
                         
-                        # Update the input with the new sample
-                        if j < width - 1:
-                            samples = samples.clone()
         return samples[:, :channels, :, :]    
     
 class random_classifier(nn.Module):
