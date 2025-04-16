4807b7bb133eb8130f4ae69bc3b222a06a81bee1
Nickmasta <58677778+Nickmasta@users.noreply.github.com>
2025-04-15 18:25:15 -0700

    Update model.py

--- model.py
+++ model.py
@@ -51,109 +51,100 @@ class PixelCNNLayer_down(nn.Module):
 
 
 class PixelCNN(nn.Module):
-    def __init__(self, nr_resnet=5, nr_filters=80, nr_logistic_mix=10,
-                    resnet_nonlinearity='concat_elu', input_channels=3, num_classes=4, embedding_dim=32):
-        # Added the number of classes in the definition 
+    def __init__(self, nr_resnet=4, nr_filters=100, nr_logistic_mix=10,
+                resnet_nonlinearity='concat_elu', input_channels=3, 
+                num_classes=4, embedding_dim=16):
         super(PixelCNN, self).__init__()
-        if resnet_nonlinearity == 'concat_elu' :
-            self.resnet_nonlinearity = lambda x : concat_elu(x)
-        else :
+        if resnet_nonlinearity == 'concat_elu':
+            self.resnet_nonlinearity = lambda x: concat_elu(x)
+        else:
             raise Exception('right now only concat elu is supported as resnet nonlinearity.')
-        # Adding the following information   
-        self.num_classes = num_classes  # Pass this as __init__ argument
-        self.embedding_dim = embedding_dim # Match network dim
-        self.class_embedding = nn.Embedding(num_classes, self.embedding_dim) ## adding our classification information
-                        
+        
+        self.num_classes = num_classes
+        self.embedding_dim = embedding_dim
+        
+        # Early fusion modifications
+        self.class_embedding = nn.Embedding(num_classes, embedding_dim)
+        # Project embedding to match spatial dimensions
+        self.embed_proj = nn.Conv2d(embedding_dim, nr_filters, kernel_size=1)
+        
         self.nr_filters = nr_filters
         self.input_channels = input_channels
         self.nr_logistic_mix = nr_logistic_mix
         self.right_shift_pad = nn.ZeroPad2d((1, 0, 0, 0))
-        self.down_shift_pad  = nn.ZeroPad2d((0, 0, 1, 0))
+        self.down_shift_pad = nn.ZeroPad2d((0, 0, 1, 0))
 
         down_nr_resnet = [nr_resnet] + [nr_resnet + 1] * 2
         self.down_layers = nn.ModuleList([PixelCNNLayer_down(down_nr_resnet[i], nr_filters,
-                                                self.resnet_nonlinearity) for i in range(3)])
+                                        self.resnet_nonlinearity) for i in range(3)])
 
-        self.up_layers   = nn.ModuleList([PixelCNNLayer_up(nr_resnet, nr_filters,
-                                                self.resnet_nonlinearity) for _ in range(3)])
+        self.up_layers = nn.ModuleList([PixelCNNLayer_up(nr_resnet, nr_filters,
+                                        self.resnet_nonlinearity) for _ in range(3)])
 
-        self.downsize_u_stream  = nn.ModuleList([down_shifted_conv2d(nr_filters, nr_filters,
-                                                    stride=(2,2)) for _ in range(2)])
+        # Modify initial convolutions to account for embedding
+        self.u_init = down_shifted_conv2d(input_channels + 1 + nr_filters, nr_filters, 
+                                        filter_size=(2,3), shift_output_down=True)
 
-        self.downsize_ul_stream = nn.ModuleList([down_right_shifted_conv2d(nr_filters,
-                                                    nr_filters, stride=(2,2)) for _ in range(2)])
-
-        self.upsize_u_stream  = nn.ModuleList([down_shifted_deconv2d(nr_filters, nr_filters,
-                                                    stride=(2,2)) for _ in range(2)])
-
-        self.upsize_ul_stream = nn.ModuleList([down_right_shifted_deconv2d(nr_filters,
-                                                    nr_filters, stride=(2,2)) for _ in range(2)])
-
-        self.u_init = down_shifted_conv2d(input_channels + 1, nr_filters, filter_size=(2,3),
-                        shift_output_down=True)
-
-        self.ul_init = nn.ModuleList([down_shifted_conv2d(input_channels + 1, nr_filters,
-                                            filter_size=(1,3), shift_output_down=True),
-                                       down_right_shifted_conv2d(input_channels + 1, nr_filters,
-                                            filter_size=(2,1), shift_output_right=True)])
+        self.ul_init = nn.ModuleList([
+            down_shifted_conv2d(input_channels + 1 + nr_filters, nr_filters,
+                              filter_size=(1,3), shift_output_down=True),
+            down_right_shifted_conv2d(input_channels + 1 + nr_filters, nr_filters,
+                                   filter_size=(2,1), shift_output_right=True)
+        ])
 
         num_mix = 3 if self.input_channels == 1 else 10
         self.nin_out = nin(nr_filters, num_mix * nr_logistic_mix)
         self.init_padding = None
 
-
     def forward(self, x, class_labels=None, sample=False):
-        #Adjusted the parameters passed into forward pass
-        # similar as done in the tf repo :
-        if self.init_padding is not sample:
-            xs = [int(y) for y in x.size()]
-            padding = Variable(torch.ones(xs[0], 1, xs[2], xs[3]), requires_grad=False)
-            self.init_padding = padding.cuda() if x.is_cuda else padding
+        if self.init_padding is None or self.init_padding.shape[0] != x.shape[0]:
+            padding = torch.ones(x.size(0), 1, x.size(2), x.size(3), device=x.device)
+            self.init_padding = padding
 
-        if sample :
-            xs = [int(y) for y in x.size()]
-            padding = Variable(torch.ones(xs[0], 1, xs[2], xs[3]), requires_grad=False)
-            padding = padding.cuda() if x.is_cuda else padding
+        if sample:
+            padding = torch.ones(x.size(0), 1, x.size(2), x.size(3), device=x.device)
             x = torch.cat((x, padding), 1)
 
-        ###      UP PASS    ###
-        x = x if sample else torch.cat((x, self.init_padding), 1)
-        # My adjustments to the forward pass, the if adds the classification info
-        if class_labels  is not None:
-            h_class = self.class_embedding(class_labels).view(-1, self.embedding_dim, 1, 1)
+        ### Early Fusion ###
+        if class_labels is not None:
+            # Get class embeddings
+            h_class = self.class_embedding(class_labels)  # [B, embedding_dim]
+            # Project to nr_filters and expand spatially
+            h_class = self.embed_proj(h_class.unsqueeze(-1).unsqueeze(-1))  # [B, nr_filters, 1, 1]
+            h_class = h_class.expand(-1, -1, x.size(2), x.size(3))  # [B, nr_filters, H, W]
         else:
-            h_class = torch.zeros(x.size(0), self.embedding_dim, 1, 1).to(x.device)
-        # Adjusting sent tensors
-        u_list  = [self.u_init(x) + h_class]
-        ul_list = [self.ul_init[0](x) + self.ul_init[1](x) + h_class]
+            # If no class provided, use zeros
+            h_class = torch.zeros(x.size(0), self.nr_filters, x.size(2), x.size(3), device=x.device)
+
+        # Prepare input with padding and class information
+        x = x if sample else torch.cat((x, self.init_padding), 1)
+        x = torch.cat((x, h_class), dim=1)  # Concatenate along channel dimension
+
+        ### UP PASS ###
+        u_list = [self.u_init(x)]
+        ul_list = [self.ul_init[0](x) + self.ul_init[1](x)]
+        
         for i in range(3):
-            # resnet block
             u_out, ul_out = self.up_layers[i](u_list[-1], ul_list[-1])
-            u_list  += u_out
+            u_list += u_out
             ul_list += ul_out
 
             if i != 2:
-                # downscale (only twice)
-                u_list  += [self.downsize_u_stream[i](u_list[-1])]
+                u_list += [self.downsize_u_stream[i](u_list[-1])]
                 ul_list += [self.downsize_ul_stream[i](ul_list[-1])]
 
-        ###    DOWN PASS    ###
-        u  = u_list.pop()
+        ### DOWN PASS ###
+        u = u_list.pop()
         ul = ul_list.pop()
 
         for i in range(3):
-            # resnet block
             u, ul = self.down_layers[i](u, ul, u_list, ul_list)
 
-            # upscale (only twice)
-            if i != 2 :
-                u  = self.upsize_u_stream[i](u)
+            if i != 2:
+                u = self.upsize_u_stream[i](u)
                 ul = self.upsize_ul_stream[i](ul)
 
         x_out = self.nin_out(F.elu(ul))
-
-        assert len(u_list) == len(ul_list) == 0, pdb.set_trace()
-
         return x_out
     
     
